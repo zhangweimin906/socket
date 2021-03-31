6a380e17738c2fc615f96f9a08dd5816a5b00a31
zhangweimin906 <zhangweimin906@163.com>
2021-03-31 16:53:24 +0800

    socket 文件提交

---
+++ Samples/ConsoleApp/Server.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Fleck.Samples.ConsoleApp
+{
+    class Server
+    {
+        static void Main()
+        {
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                        {
+                            Console.WriteLine("Open!");
+                            allSockets.Add(socket);
+                        };
+                    socket.OnClose = () =>
+                        {
+                            Console.WriteLine("Close!");
+                            allSockets.Remove(socket);
+                        };
+                    socket.OnMessage = message =>
+                        {
+                            if (message == "james")
+                            {
+                                message = "真棒";
+                            }
+                            else if(message == "1")
+                            {
+                                message = "您输入的是1";
+                            }
+                            
+                            Console.WriteLine(message);
+                            allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                        };
+                });
+
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
